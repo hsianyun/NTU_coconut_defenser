8c0b0abfad83e5d56ff28b41c3a15255402704d3
ted <tedcheng0904@gmail.com>
2022-12-27 16:05:14 +0800

    modify defenser

---
+++ DEFENSER.py
@@ -0,0 +1,45 @@
+
+class Defenser:
+    def __init__(self):
+        pass
+
+
+class Suger(Defenser):
+    def __init__(self):
+        super().__init__()
+        self.rate_of_fire = 10
+        self.range = 0
+        self.price = 25
+
+class JP(Defenser):
+    def __init__(self):
+        super().__init__()
+        self.rate_of_fire = 25
+        self.range = 0
+        self.price = 30
+
+class Golden(Defenser):
+    def __init__(self):
+        super().__init__()
+        self.rate_of_fire = 15
+        self.range = 0
+        self.price = 80
+
+class King(Defenser):
+    def __init__(self):
+        super().__init__()
+        self.rate_of_fire = 5
+        self.range = 0
+        self.price = 120
+
+class Ice(Defenser):
+    def __init__(self):
+        super().__init__()
+        self.rate_of_fire = 20
+        self.range = 0
+        self.price = 60
+
+
+
+
+
