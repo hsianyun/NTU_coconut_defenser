9fb9fb5e9bea6a6cc1b610fef014887c5e61c1d3
KWJ <kwj930521@gmail.com>
2022-12-28 20:59:59 +0800

    attacker

--- ATTACKER.py
+++ ATTACKER.py
@@ -16,17 +16,29 @@ import os
 class Attacker:
     def __init__(self):
         self.damage = 0
+        self.width = ##
+        self.height = ##
+        self.path = ##
+        self.x = self.path[0][0]
+        self.y = self.path[0][1]
+
+
+    # 畫圖
+    def draw(self,win):
+        self.img = ##
+        win.blit(self.img,(self.x - self.img.get_width()/2, self.y- self.img.get_height()/2))
+    
+    # 移動路徑
+    def move(self):
 
     # 受到攻擊扣血的機制並偵測是否死亡
-    def getdamage(self,damage):
+    def hit(self,damage):
         self.damage += damage
         if self.damage >= self.ini_blood:
-            print("GG") # 死亡
-            attackers.remove(self)
+            attackers.remove(self) # 死亡
         if damage == 1:
-            pass       #緩速
+            pass       # 停30clk
 
-    # 移動路徑
 
     # 血量歸零時死亡的機制
 
@@ -39,6 +51,7 @@ class Pedestrian(Attacker):
         self.price = 2
         self.ini_blood = 10
         self.power = 1
+        self.speed = 1
 
 
 class Bicycle(Attacker):
@@ -47,6 +60,7 @@ class Bicycle(Attacker):
         self.price = 10
         self.ini_blood = 30
         self.power = 2
+        self.speed = 2
 
 class Skateboard(Attacker):
     def __init__(self):
@@ -54,6 +68,7 @@ class Skateboard(Attacker):
         self.price = 4
         self.ini_blood = 10
         self.power = 1
+        self.speed = 4
 
 class Car(Attacker):
     def __init__(self):
@@ -61,6 +76,7 @@ class Car(Attacker):
         self.price = 50
         self.ini_blood = 100
         self.power = 5
+        self.speed = 2
 
 class Shui_yuan_car(Attacker):
     def __init__(self):
@@ -68,6 +84,7 @@ class Shui_yuan_car(Attacker):
         self.price = 90
         self.ini_blood = 100
         self.power = 8
+        self.speed = 1
 
 class Ambulance(Attacker):
     def __init__(self):
@@ -75,6 +92,7 @@ class Ambulance(Attacker):
         self.price = 80
         self.ini_blood = 100
         self.power = 8
+        self.speed = 2
 
 class Student_Association(Attacker):
     def __init__(self):
@@ -82,5 +100,6 @@ class Student_Association(Attacker):
         self.price = 6
         self.ini_blood = 20
         self.power = 1
+        self.speed = 2
 
 
